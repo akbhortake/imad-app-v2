f7f576bc42a5fc429d306bbd2b0188f7e0662633
akbhortake <akbhortake@gmail.com>
2017-02-15 22:34:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,5 @@
 var element=document.getElementById("main_containt");
 element.innerHTML="jal an dhur sangatach";
 
-var img=docuemnt
+var img =document.getElementById("madi");
+img.onclick=function(){ img.style.marginleft="100px"; };
