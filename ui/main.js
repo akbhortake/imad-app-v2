6d82d5d5fdfb14d9c9517453431dc025fe0508a9
akbhortake <akbhortake@gmail.com>
2017-02-16 12:46:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ var count=0;
 function getCount()
 {
     count=1+count;
-    doucument.getelementbyid("counter").getinnerhtml=count.toString();
+    document.getElementById("counter").innerHTML=count.toString();
 }
